fadcd22217bc290cf6654047c1ed655bb96d664e
Alec Jang <ayj.max@gmail.com>
2024-10-19 13:53:27 -0400

    Added example routes and starting login page

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,19 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/">
       <Routes>
-        <Route path="/" element={<>Hello!!</>}/> {/* 👈 Renders at /app/ */}
+        <Route path="/" element={<>Hello!!</>}/>
+        {/* EXAMPLES OF ROUTES (DON'T NEED TO USE THESE)
+        <Route path='/' element={<>Hello!!!</>} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={  <Dashboard/> } />
+        <Route path='/compatability-checker' element={  <Checker/>} />
+        <Route path='/gemini-chat-bot' element={  <ChatBotPage/> } />
+        <Route path='/history' element={ <History/>  }/>
+        <Route path='/settings' element={ <Settings/>  } />
+        <Route path='/my-prescriptions' element={<Dashboard/>} />
+        <Route path="*" element={<NotFound />} />
+        */}
       </Routes>
     </BrowserRouter>
   </StrictMode>,
