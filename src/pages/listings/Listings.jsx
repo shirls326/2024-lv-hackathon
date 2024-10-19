ce77c6c40e0677bf12eeb15c7e7777dbc79031ed
Jon Klein <jonklein2021@gmail.com>
2024-10-19 14:06:29 -0400

    begin pullinh from firebase into listings

--- src/pages/listings/Listings.jsx
+++ src/pages/listings/Listings.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 function Listings() {
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+    // fetch products from firebase
+    const ref = firebase.database().ref('products');
+    ref.on('value', snapshot => {
+      const state = snapshot.val();
+      setProducts(state);
+    });
+  });
+
+
   return (
-    <div>Listings</div>
+  <>
+    <h1>Listings</h1>
+    <div className="listings">
+      {products.map(product => (
+        <div key={product.id} className="product">
+          <h2>{product.name}</h2>
+          <p>{product.desc}</p>
+          <p>{product.price}</p>
+        </div>
+      ))}
+    </div>
+  </>
   )
 }
 
