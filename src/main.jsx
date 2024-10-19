a7fdcbec116f55fe3ef31e962c61eaa6378318b4
Alec Jang <ayj.max@gmail.com>
2024-10-19 15:23:43 -0400

    Updated login page

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Login
+import Login from './pages/login/login';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<>Hello!!</>}/>
+        <Route path="/login" element={<Login />} />
         {/* EXAMPLES OF ROUTES (DON'T NEED TO USE THESE)
         <Route path='/' element={<>Hello!!!</>} />
         <Route path='/signup' element={<Signup />} />
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getDatabase, ref, set, get} from "firebase/database";
-
+import GradientSVG from '/login_gradient.svg?url';
 
 function Login() {
-  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  return (
+    <div className='flex justify-center max-h-screen w-screen bg-white'>
+      <img alt="bruh" src={GradientSVG} className='h-screen w-auto'></img>
+      <>Hello Login!!</>
+    </div>
+  );
 }
+
+export default Login;
