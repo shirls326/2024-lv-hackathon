0c2d02f7537e974991372ca3e780be9804d260f7
Jon Klein <jonklein2021@gmail.com>
2024-10-19 15:20:34 -0400

    get post new product working

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
+// react
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Listings from './pages/listings/Listings';
+
+// pages
 import Landing from './pages/landing/Landing';
-import NotFound from './pages/404/NotFound';
+import Listings from './pages/listings/Listings';
 import Product from './pages/product/Product';
+import NewProduct from './pages/newproduct/NewProduct';
+import NotFound from './pages/404/NotFound';
+
+// styles
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,6 +20,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Landing />} />
         <Route path="/products" element={<Listings />} />
         <Route path="/products/:id" element={<Product />} />
+        <Route path="/newproduct/" element={<NewProduct />} />
         <Route path="*" element={<NotFound />} />
         {/* EXAMPLES OF ROUTES (DON'T NEED TO USE THESE)
         <Route path='/' element={<>Hello!!!</>} />
