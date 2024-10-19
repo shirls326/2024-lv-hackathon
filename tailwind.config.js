ece8f35378de776a626446d2bab40aa1d322dde5
Alec Jang <ayj.max@gmail.com>
2024-10-19 15:24:34 -0400

    Updated the import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+import daisyui from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
 }
 
