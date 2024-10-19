d620ee166f4411c380ac54f330ec93ae95382b8b
Alec Jang <ayj.max@gmail.com>
2024-10-19 18:11:03 -0400

    Frontend for login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -8,13 +8,28 @@ const InputTitle = ({ children }) => {
   return <h2 className='mb-[0.2rem] mt-[1rem] font-thin'>{children}</h2>
 }
 
-
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [theme, setTheme] = useState('light');
   const navigate = useNavigate();
+
+  const handleEmailChange = (event) => {
+    event.preventDefault();
+    const val = event.target.value;
+    if (val) {
+      setEmail(val);
+    }
+  }
+
+  const handlePasswordChange = (event) => {
+    event.preventDefault();
+    const val = event.target.value;
+    if (val) {
+      setPassword(val);
+    }
+  }
 
 
   const handleLogin = async (e) => {
@@ -42,15 +57,21 @@ function Login() {
     <div data-theme={theme} className='flex justify-between items-center max-h-screen w-screen bg-white'>
       <img alt="bruh" src={GradientSVG} className='h-screen w-auto'></img>
       <div className='flex flex-col justify-center w-full h-screen mx-[7rem] gap-[0.5rem]'>
-        <h1 className='mx-auto font-thin'>Sign Up</h1>
-        <div className='flex flex-row justify-between'>
-          <div className=''>
-            <InputTitle>First Name</InputTitle>
-            <label className="input input-bordered flex items-center gap-2 border-2 border-[#717171] bg-[#f8f8f8]">
-              <input type="text" className="grow" onChange={handleFirstNameChange} />
-            </label>
-          </div>
+        <h1 className='mx-auto font-thin'>Login</h1>
+        <div>
+          <InputTitle>Email</InputTitle>
+          <label className="input input-bordered flex items-center gap-2 border-2 border-[#717171] bg-[#f8f8f8]">
+            <input type="text" className="grow" onChange={handleEmailChange} />
+          </label>
         </div>
+        <div>
+          <InputTitle>Password</InputTitle>
+          <label className="input input-bordered flex items-center gap-2 border-2 border-[#717171] bg-[#f8f8f8]">
+            <input type="password" className="grow" onChange={handlePasswordChange} />
+          </label>
+        </div>
+
+        <button onClick={handleLogin} className='btn btn-success mt-4'>Login</button>
       </div>
     </div>
   );
