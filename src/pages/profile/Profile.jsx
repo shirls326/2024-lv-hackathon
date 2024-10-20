90c9eb821989d7f8bb2d90aa642fb2cdfc9af4f2
Jon Klein <jonklein2021@gmail.com>
2024-10-20 08:08:45 -0400

    profile edit working

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react'
 // Firebase
 import { auth, db } from '../../firebase/config';
-import { query, orderByChild, equalTo, ref, get } from "firebase/database";
+import { query, orderByChild, equalTo, ref, get, set } from "firebase/database";
 import { onAuthStateChanged } from 'firebase/auth';
 // Components
 import Navbar from '../../components/Navbar';
@@ -10,8 +10,10 @@ import Sidebar from '../../components/Sidebar';
 // Icon
 import EditSVG from '/edit_icon.svg?url';
 import LoadingSkeleton from '../../components/LoadingSkeleton';
+import placeholder from '../../assets/product_placeholder.svg';
 
 function Profile() {
+  const [userId, setUserId] = useState(null);
   const [userData, setUserData] = useState(null);
   const [userProducts, setUserProducts] = useState([]);
   const [userDataLoading, setUserDataLoading] = useState(true);
@@ -21,12 +23,12 @@ function Profile() {
   async function getUserByEmail(email) {
     const usersRef = ref(db, 'users'); // Reference to the users node
     const emailQuery = query(usersRef, orderByChild('email'), equalTo(email)); // Query by email
-    
+
     try {
       const snapshot = await get(emailQuery); // Fetch the snapshot
       if (snapshot.exists()) {
-        const userData = Object.values(snapshot.val())[0]; // Extract user data
-        setUserData(userData); // Set the user data in state
+        const userData = Object.values(snapshot.val())[0]; // Extract user data  
+        setUserData(userData); // Set the user data in state, add ID
         console.log("User data found: ", userData);
       } else {
         console.log("No user found with the email:", email);
@@ -39,14 +41,14 @@ function Profile() {
   }
 
   async function getProductsByUserID(userID) {
-    const productsRef = ref(db, 'products'); 
+    const productsRef = ref(db, 'products');
     const userProductQuery = query(productsRef, orderByChild('seller'), equalTo(userID));
-    
+
     try {
       const snapshot = await get(userProductQuery); // Fetch the snapshot
       if (snapshot.exists()) {
         const data = Object.values(snapshot.val())[0];
-        setUserProducts(data); 
+        setUserProducts(data);
         console.log("Data found: ", data);
       } else {
         console.log("No products found with the userID:", userID);
@@ -63,6 +65,7 @@ function Profile() {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         console.log("User email", user.email);
+        setUserId(user.uid);
         getUserByEmail(user.email); // Fetch user data using the email
         getProductsByUserID(user.uid)
       } else {
@@ -84,8 +87,8 @@ function Profile() {
   }
   return (
     <>
-      <Sidebar/>
-      <Navbar/>
+      <Sidebar />
+      <Navbar />
 
       <div data-theme="light" className="pl-[20vw] pt-[9vh] !max-w-screen !h-screen box-border">
         {/* Profile Section */}
@@ -94,11 +97,11 @@ function Profile() {
           <div className="flex items-center">
             <div className="avatar">
               <div className="w-[26vh] rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                <img src="https://placehold.co/100" alt="Profile" />
+                <img src={placeholder} alt="Profile" />
               </div>
             </div>
           </div>
-          
+
           {/* User Info */}
           <div className='flex flex-col gap-6'>
             <div className="text-3xl">
@@ -117,7 +120,7 @@ function Profile() {
 
           {/* Edit Icon */}
           <div className='flex flex-col h-full pt-5 box-border'>
-            <button className="btn btn-square btn-sm" onClick={()=>document.getElementById('edit_profile_modal').showModal()}>
+            <button className="btn btn-square btn-sm" onClick={() => document.getElementById('edit_profile_modal').showModal()}>
               <img src={EditSVG} />
             </button>
           </div>
@@ -142,7 +145,7 @@ function Profile() {
           </div>
         </div>
 
-        <EditProfileModal />
+        <EditProfileModal userData={{ uid: userId, ...userData }} />
       </div>
     </>
   );
@@ -153,16 +156,45 @@ const InputTitle = ({ children }) => {
   return <h2 className='mb-[0.2rem] mt-[1rem] font-thin'>{children}</h2>
 }
 // eslint-disable-next-line react/prop-types
-const TextInput = ({ children }) => {
+const TextInput = ({ children, getter, setter }) => {
   return <>
     <InputTitle>{children}</InputTitle>
     <label className="input input-bordered flex items-center gap-2 border-2 border-[#717171] bg-[#f8f8f8]">
-      <input type="text" className="grow"/>
+      <input
+        type="text"
+        value={getter}
+        onChange={e => setter(e.target.value)}
+        className="grow"
+      />
     </label>
   </>
 }
 
-function EditProfileModal () {
+function EditProfileModal({ userData }) {
+  console.log(userData);
+
+  const [firstName, setFirstName] = useState(userData.firstName);
+  const [lastName, setLastName] = useState(userData.lastName);
+
+  function handleSave() {
+    console.log("Saving profile changes to DB");
+
+    // update user data in DB
+    const userRef = ref(db, 'users/' + userData.uid);
+
+    set(userRef, {
+      firstName: firstName,
+      lastName: lastName,
+      uniID: userData.uniID,
+      university: userData.university,
+      email: userData.email,
+      verified: userData.verified
+    }, { merge: true });
+
+    // close the modal
+    document.getElementById('edit_profile_modal').close();
+  }
+
   return <>
     <button className="btn" ></button>
     <dialog id="edit_profile_modal" className="modal">
@@ -175,19 +207,14 @@ function EditProfileModal () {
 
         <div className='flex flex-col justify-center gap-1'>
           <div>
-            <TextInput>First Name</TextInput>
+            <TextInput init={userData.firstName} getter={firstName} setter={setFirstName}>First Name</TextInput>
           </div>
           <div>
-            <TextInput>Last Name</TextInput>
-          </div>
-          <div>
-            <InputTitle>Password</InputTitle>
-            <label className="input input-bordered flex items-center gap-2 border-2 border-[#717171] bg-[#f8f8f8]">
-              <input type="password" className="grow" />
-            </label>
+            <TextInput init={userData.lastName} getter={lastName} setter={setLastName}>Last Name</TextInput>
           </div>
 
-          <button className='btn btn-success mt-4'>Save Changes</button>
+          {/* save to DB on submission */}
+          <button className='btn btn-success mt-4' onClick={handleSave}>Save Changes</button>
         </div>
         <p className="py-4">Press ESC key or click on ✕ button to close</p>
       </div>
