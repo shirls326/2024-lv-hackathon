9f96be09ec7b531445dbb339387a76363aa15668
Shirley Angos <shirleyangos@ShirleyMac-5.local>
2024-10-19 22:46:55 -0400

    jon

--- src/components/cardListing.jsx
+++ src/components/cardListing.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import './css/cardListing.css';
-import flowerTest from './flowerTest.jpeg';
 
 function cardListing() {
 return (
     <div className="ad-container">
         <div className='adImg'>
-            <img src= {flowerTest} alt="listing image"/>   
-            
-            
+            <img src="https://picsum.photos/200" alt="listing image"/>   
         </div>
 
         <div className='adInfo'> 
             <p>ITEM LISTING</p>
             <p className='price'>$XXX.XX</p>
         </div>
-        
-        
-
-        {/* <img 
-            src='https://via.placeholder.com/150'
-            alt="Kitchen Aid Mixer"
-            className="ad-image"
-        />
-        <div className="ad-overlay">
-            <h2>Kitchen Aid Mixer</h2>
-            <p className="ad-price">$25</p>
-        </div> */}
     </div>
 );
 };
