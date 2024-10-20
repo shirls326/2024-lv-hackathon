d2cd0dcfda5c7659bb476adf7b39f4268042419f
Alec Jang <ayj.max@gmail.com>
2024-10-20 03:20:24 -0400

    Added redirects for Navbar and Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,20 @@
 import './css/Sidebar.css';
+import { useNavigate } from 'react-router'
 
 function Sidebar() {
+    const navigate = useNavigate();
+    const handleRedirect = (event) => {
+        event.preventDefault();
+        const button = event.currentTarget;
+        if (button && button.dataset && button.dataset.redirect) {
+            navigate(button.dataset.redirect);
+        }
+    }
     return (
         <div className='sidebar'>
-
             <div className='sidebar top'>
-                <button>Saved Listings</button>
-                <button>Creating Listing</button>
+                <button data-redirect="/profile" onClick={handleRedirect}>Saved Listings</button>
+                <button data-redirect="/newproduct" onClick={handleRedirect}>Creating Listing</button>
             </div>
             <hr />
             <h2>Categories</h2>
@@ -15,9 +23,7 @@ function Sidebar() {
                 <button>Furniture</button>
                 <button>Free Stuff</button>
                 <button>Kitchen</button>
-
             </div>
-          
         </div>
     )
 }
